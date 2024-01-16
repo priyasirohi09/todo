85ef1f0b0a4559d2a0660f301d652bd44467666f
YOUR NAME <YOUR@EMAIL.com>
2024-01-16 23:12:55 +0530

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   resources :todo_lists do
-    resources :todo_items
-   end
+    resources :todo_items do
+      member do
+        get 'complete'
+      end
+    end
+  end
    root "todo_lists#index"
 
   # Defines the root path route ("/")
